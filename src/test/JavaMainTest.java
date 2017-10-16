a1445429cf27a8a7ed1c401dfc895e1a9267f0d2
wujun <jun.wu@lrlz.com>
2017-10-16 16:43:07 +0800

    Integer的范围值

--- src/test/JavaMainTest.java
+++ src/test/JavaMainTest.java
@@ -1,12 +1,17 @@
 package test;
 
-import test.manager.ThreadManager;
 import test.utils.LogUtil;
 
 public class JavaMainTest {
 
     public static void main(String[] args) {
         LogUtil.log("大俊子在此!");
-        new ThreadManager().start();
+//        new ThreadManager().start();
+        testInteger();
+    }
+
+    private static void testInteger() {
+        LogUtil.log("Integer最大值:" + Integer.MAX_VALUE);
+        LogUtil.log("Integer最小值:" + Integer.MIN_VALUE);
     }
 }
