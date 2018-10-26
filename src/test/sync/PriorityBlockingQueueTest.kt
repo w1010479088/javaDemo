203ee039a43eaf34b9122b0c437e81218c32f195
mac <>
2018-10-26 18:02:57 +0800

    1.小改动

---
+++ src/test/sync/PriorityBlockingQueueTest.kt
@@ -0,0 +1,19 @@
+package test.sync
+
+import test.utils.LogUtil
+import java.util.concurrent.PriorityBlockingQueue
+
+fun main(args: Array<String>) {
+    PriorityBlockingQueueTest().test()
+}
+
+class PriorityBlockingQueueTest {
+    fun test() {
+        val blockingQueue = PriorityBlockingQueue<Runnable>(3)
+        blockingQueue.put(Runnable {  })
+    }
+}
+
+private fun log(content: String) {
+    LogUtil.log(content)
+}
