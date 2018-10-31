bbc913f751d16b9c2fcb2447c002a1562c70ea2a
mac <>
2018-10-31 18:31:59 +0800

    1.策略模式 2.职责链模式

---
+++ src/test/design/responbility/Test.kt
@@ -0,0 +1,54 @@
+package test.design.responbility
+
+import test.utils.LogUtil
+
+fun main(args: Array<String>) {
+    val task = Task(150)
+    val handler1 = Handler1("1号")
+    val handler2 = Handler2("2号")
+    val handler3 = Handler3("3号")
+
+    handler1.nextHandler(handler2)
+    handler2.nextHandler(handler3)
+
+    handler1.execute(task)
+}
+
+data class Task(val price: Int)
+
+abstract class BaseHandler(val name: String) {
+    private var next: BaseHandler? = null
+
+    fun nextHandler(next: BaseHandler) {
+        this.next = next
+    }
+
+    fun execute(task: Task) {
+        if (needHandle(task)) {
+            log("$name 处理了订单:${task.price}")
+        } else {
+            next?.execute(task)
+        }
+    }
+
+    abstract fun needHandle(task: Task): Boolean
+}
+
+class Handler1(name: String) : BaseHandler(name) {
+
+    override fun needHandle(task: Task) = task.price < 100
+}
+
+class Handler2(name: String) : BaseHandler(name) {
+
+    override fun needHandle(task: Task) = task.price < 1000
+}
+
+class Handler3(name: String) : BaseHandler(name) {
+
+    override fun needHandle(task: Task) = task.price < 10000
+}
+
+private fun log(content: String) {
+    LogUtil.log(content)
+}
---
+++ src/test/design/strategy/StrategyTest.kt
@@ -0,0 +1,60 @@
+package test.design.strategy
+
+import test.utils.LogUtil
+
+const val STUDENT_DISCOUNT = 0.6F
+const val CHILD_DISCOUNT = 0.2F
+const val VIP_DISCOUNT = 0.5F
+
+fun main(args: Array<String>) {
+    val system = TicketSystem()
+    val student = StudentDiscount()
+    val child = ChildDiscount()
+    val vip = VipDiscount()
+
+    val price = 60F
+    val priceStu = system.getPrice(price, student)
+    val priceChild = system.getPrice(price, child)
+    val priceVip = system.getPrice(price, vip)
+
+    log(priceStu)
+    log(priceChild)
+    log(priceVip)
+}
+
+class TicketSystem {
+
+    fun getPrice(price: Float, discount: IDiscount): Float {
+        return discount.calculate(price)
+    }
+}
+
+interface IDiscount {
+
+    fun calculate(price: Float): Float
+}
+
+class StudentDiscount : IDiscount {
+
+    override fun calculate(price: Float): Float {
+        return price * STUDENT_DISCOUNT
+    }
+}
+
+class ChildDiscount : IDiscount {
+
+    override fun calculate(price: Float): Float {
+        return price * CHILD_DISCOUNT
+    }
+}
+
+class VipDiscount : IDiscount {
+
+    override fun calculate(price: Float): Float {
+        return price * VIP_DISCOUNT
+    }
+}
+
+fun log(price: Float) {
+    LogUtil.log(price.toString())
+}
