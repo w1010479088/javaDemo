0cae23008c85c732c5ca04112fa1a3f40e13d0b3
mac <>
2018-11-09 15:37:26 +0800

    1.并发的问题

--- src/test/JavaMainTest.java
+++ src/test/JavaMainTest.java
@@ -1,10 +1,16 @@
 package test;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import test.strategy.Calcluater;
 import test.utils.LogUtil;
 
 public class JavaMainTest {
     private static int count;
+
     public static void main(String[] args) {
         testMethod();
     }
@@ -35,5 +41,21 @@ public class JavaMainTest {
 
     public synchronized int getB() {
         return 2;
+    }
+
+    private void testFuture() {
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        Future<Integer> future = service.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return null;
+            }
+        });
+        boolean futureDone = future.isDone();
+        try {
+            Integer result = future.get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
