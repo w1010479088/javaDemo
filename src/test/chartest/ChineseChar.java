59107a1950d85c9e961e0b3b0d0e4596b718b29f
mac <1010479088@qq.com>
2019-04-22 16:32:25 +0800

    1.中文字符截取问题
        private int charLen(char charContent) {
            return String.valueOf(charContent).getBytes(getCharSet()).length;
        }
    
        private Charset getCharSet() {
            return Charset.forName("gbk");
        }
    
    2.喝水的问题

--- src/test/chartest/ChineseChar.java
+++ src/test/chartest/ChineseChar.java
@@ -5,6 +5,8 @@ import java.nio.charset.Charset;
 import test.util.LogUtil;
 
 public class ChineseChar {
+    //中文字符问题，在不同平台byte占位不同
+
     private static final String TEST_CHAR = "我是ABC的测！";
 
     public static void main(String[] args) {
@@ -12,7 +14,7 @@ public class ChineseChar {
         log("结果：" + result);
     }
 
-    private String slice(String content, int bytePos) {
+    public String slice(String content, int bytePos) {
         StringBuilder sb = new StringBuilder();
         int curBytePos = 0;
         char[] chars = content.toCharArray();
@@ -38,5 +40,31 @@ public class ChineseChar {
 
     private static void log(String content) {
         LogUtil.log(content);
+    }
+}
+
+
+class BottleTest {
+    //喝汽水的问题
+
+    public static void main(String[] args) {
+        new BottleTest().swap(4);
+    }
+
+    private int count = 0;
+    private int left_bottle = 0;
+    private static final int RATIO = 2;
+
+    public void swap(int money) {
+        count += money;
+        left_bottle += money;
+
+        if (left_bottle >= RATIO) {
+            int tempMoney = left_bottle / RATIO;
+            left_bottle = left_bottle % RATIO;
+            swap(tempMoney);
+        } else {
+            LogUtil.log(String.format("兑换%2d,剩余瓶子%2d", count, left_bottle));
+        }
     }
 }
