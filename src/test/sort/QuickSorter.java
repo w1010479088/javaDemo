a615cf7aece31456d1bd403e8de020e04fa6ec05
mac <1010479088@qq.com>
2019-04-23 10:38:15 +0800

    1.快速排序

--- src/test/sort/QuickSorter.java
+++ src/test/sort/QuickSorter.java
@@ -1,9 +1,9 @@
 package test.sort;
 
 import test.util.LogUtil;
+import test.util.SwapUtil;
 
 public class QuickSorter implements ISortable {
-    private static final int[] TEST = new int[]{8, 3, 1, 4, 5, 2};
 
     public static void main(String[] args) {
         int[] sorted = new QuickSorter().sort(TEST);
@@ -12,6 +12,49 @@ public class QuickSorter implements ISortable {
 
     @Override
     public int[] sort(int[] content) {
-        return new int[0];
+        sort(TEST, 0, TEST.length - 1);
+        return TEST;
+    }
+
+    private static final int[] TEST = {12, 20, 1, 5};
+
+    private void sort(int[] content, int low, int high) {
+        int start = low;
+        int end = high;
+        int key = content[low];
+
+        //从后往前
+        while (start < end) {
+            while (start < end) {
+                if (content[end] < key) {
+                    SwapUtil.swap(content, start, end);
+                    break;
+                } else {
+                    end--;
+                }
+            }
+
+            LogUtil.log(content);
+
+            //从前往后
+            while (start < end) {
+                if (content[start] > key) {
+                    SwapUtil.swap(content, start, end);
+                    break;
+                } else {
+                    start++;
+                }
+            }
+
+            LogUtil.log(content);
+        }
+
+        if (start > low) {
+            sort(content, low, start - 1);
+        }
+
+        if (end < high) {
+            sort(content, end + 1, high);
+        }
     }
 }
--- src/test/util/LogUtil.java
+++ src/test/util/LogUtil.java
@@ -7,11 +7,14 @@ public class LogUtil {
     }
 
     public static void log(int[] content) {
-        StringBuilder sb = new StringBuilder();
-        for (int item : content) {
-            sb.append(String.valueOf(item));
-            sb.append("\n");
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < content.length; i++) {
+            sb.append(String.valueOf(content[i]));
+            if (i < content.length - 1) {
+                sb.append(",");
+            }
         }
+        sb.append("]");
         LogUtil.log(sb.toString());
     }
 }
