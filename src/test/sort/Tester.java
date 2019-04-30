a16a6a1082c331c13cd176668d6ae5818ffe7ec8
mac <1010479088@qq.com>
2019-04-30 11:08:26 +0800

    1.小改动

--- src/test/sort/Tester.java
+++ src/test/sort/Tester.java
@@ -6,6 +6,7 @@ public class Tester {
     private static final int[] TEST = new int[]{8, 3, 1, 4, 5, 2};
 
     public static void main(String[] args) {
-        LogUtil.log(new InsertSorter().sort(TEST));
+        new InsertSorter().sort(TEST);
+        LogUtil.log(TEST);
     }
 }
