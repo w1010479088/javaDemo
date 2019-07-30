6b31254c65604c7860db04774855698a29f0a860
mac <1010479088@qq.com>
2019-07-30 09:28:43 +0800

    1.A BubbleSorter 冒泡排序

--- src/test/sort/BubbleSorter.java
+++ src/test/sort/BubbleSorter.java
@@ -9,7 +9,7 @@ public class BubbleSorter implements ISortable {
         for (int i = 0; i < content.length - 1; i++) {
             for (int j = 0; j < content.length - 1 - i; j++) {
                 if (content[j] > content[j + 1]) {
-                    SwapUtil.swap(content, j, j + 1);
+                    SwapUtil.swap(content, j,  j + 1);
                 }
             }
         }
