85ecdd64bccbb21870702130c316702bad10d207
mac <1010479088@qq.com>
2019-09-08 22:34:26 +0800

    1.Lock必须final，不能改变

--- src/job/lock/Lock.java
+++ src/job/lock/Lock.java
@@ -6,6 +6,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Lock {
     private final java.util.concurrent.locks.Lock mLock = new ReentrantLock();
     private final Condition mCondition = mLock.newCondition();
+    private final Object mLockObj = new Object();
 
     private void a() {
         mLock.tryLock();
@@ -22,5 +23,11 @@ public class Lock {
         mLock.lock();
         mCondition.signal();
         mLock.unlock();
+    }
+
+    private void c() {
+        synchronized (mLockObj){
+            //mLockObj必须是final类型的
+        }
     }
 }
